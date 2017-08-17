a32fe14b1cfffd797aa8d9bb77811e114c8f3c16
Benjamin Bressler and John Parsons <benjamin@>
2017-08-17 13:23:42 -0700

    add minor design elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// TODO 
+// TODO
 //      2. Add functionality to numbers >= 4000 && numbers <= 3,999,999 qqqqqq
 
 // BACK END LOGIC
@@ -7,7 +7,7 @@ function convert(num) {
   var roman="";
   var romanNumeral = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
   var arabic = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
-  for( i=0;i<arabic.length;i++){
+  for(i=0;i<arabic.length;i++){
     while(num>=arabic[i]){
       roman = roman + romanNumeral[i];
       num = num - arabic[i];
@@ -24,7 +24,7 @@ function empty() {
     var x;
     x = document.getElementById("userInput").value;
     if (x <= 0 || x >= 4000) {
-        alert("Please enter a number between 1 and 3,999");
+        alert("Please enter an integer between 1 and 3,999");
         return false;
     };
 }
